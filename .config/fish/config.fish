e32710252b61d37b70fb6d55d893e73b508f27ec
ultramoley <ultramoley@pm.me>
2025-07-09 11:51:57 +0100

    First commit - CachyOS base

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+﻿## Set values
+function fish_greeting
+end
+
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+
+# Navigation
+alias cdd='cd ~/vault'
+
+# Neovim
+alias vi='nvim'
+alias vim='nvim'
+alias v='nvim .'
